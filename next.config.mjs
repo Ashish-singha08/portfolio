32ca3b8f0259bcaf55f12df5cd23302a7820f330
Ashish Singhal <ashish@Ashishs-MacBook-Air.local>
2026-02-22 00:52:28 +0530

    update redirect

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,17 @@
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'portfolio-sigma-silk-qnev14chxk.vercel.app' }],
+        destination: 'https://ashishsinghal.dev/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default nextConfig
